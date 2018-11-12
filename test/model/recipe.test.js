3289c1e5fa07dcc333ece24fd63e4e24fe274c17
Jack Branch <jackbranch@ECS0821.local>
2018-11-12 15:16:51 +0000

    adding model and model testing

---
+++ test/model/recipe.test.js
@@ -0,0 +1,25 @@
+var Recipe = require('../../lib/model/recipe');
+
+const testName = 'testName';
+const testTime = '11:00';
+const testIngredients = ['sugar', 'flower', 'butter'];
+const testMethod = 'testMethod';
+const testUserId = 'testUserId';
+
+var testRecipe = new Recipe(testName, testTime, testIngredients, testMethod);
+
+test('Name is correct', () => {
+	expect(testRecipe.name).toEqual(testName);
+});
+
+test('Time is correct', () => {
+	expect(testRecipe.time).toEqual(testTime);
+});
+
+test('Ingredients are correct', () => {
+	expect(testRecipe.ingredits).toEqual(testIngredients);
+});
+
+test('Method is correct', () => {
+	expect(testRecipe.method).toEqual(testMethod);
+});
