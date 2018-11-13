00a39d51097668255bd6be88a7ee7d3680d8a8a2
Jack Branch <jackbranch@ECS0821.local>
2018-11-13 14:52:27 +0000

    adding integration test

--- tests/dBintegration.test.js
+++ tests/dBintegration.test.js
@@ -3,31 +3,38 @@
  * database
  * 
  */
+const User = require('../lib/model/user');
+const dbSetup = require('./helpers/dbSetupScripts');
+const monk = require('monk');
+const db = monk('localhost:27017/sgTest'); // set to sg for dev db
+const users = db.get('users')
+const recipes = db.get('recipes');
 describe('Database Integration Tests', function() {
 
-	beforeAll(function() {
-		
+	afterAll(() => {
+		dbSetup.clearTestData();
 	});
 
-	afterAll(function() {
+	let username = 'testUsername';
 
+	function insertDocument(doc, collection) {
+		collection.insert(doc).then(() => db.close());
+	}
+
+	it('Server should send a User', () => {
+		let testUser = new User(username, '', '', '');
+		users.insert(testUser)
+			.then((doc) => {
+				db.close();
+				expect(doc['username']).toEqual(testUser.username);
+			});
 	});
 
-	beforeEach(function() {
-
-	});
-
-	afterEach(function() {
-
-	});
-
-	it('Server should connect to the Db', function() {
-		
-	});
-	it('Server should send a document', function() {
-
-	});
-	it('Server should recievce an document', function() {
-
-	});
+	// it('Server should recievce a User', () => {
+	// 	users.findOne({username: username}, 'username')
+	// 		.then((doc) => {
+	// 			db.close();
+	// 			expect(doc).toEqual(0);
+	// 		})
+	// });	
 });
---
+++ tests/helpers/dbSetupScripts.js
@@ -0,0 +1,35 @@
+const User = require('../../lib/model/user');
+const Recipe = require('../../lib/model/recipe');
+
+const monk = require('monk');
+let db = monk('localhost:27017/sgTest'); // set to sg for dev db
+let users = db.get('users');
+
+var testUserList = [
+	new User('testUser1', 'testPassword1', 'testFirstName1', 'testLastName1'),
+	new User('testUser2', 'testPassword2', 'testFirstName2', 'testLastName2'),
+	new User('testUser3', 'testPassword3', 'testFirstName3', 'testLastName3')
+];
+
+var databaseSetup = {
+
+	addTestData: function() {
+		users.insert(testUserList)
+			.catch((err) => {
+				console.log(err);
+            }).then(() => db.close());
+	},
+	clearTestData: function() {
+		users.drop()
+			.catch((err) => {
+				console.log(err);
+            }).then(() => db.close());
+	},
+};
+
+
+// databaseSetup.clearTestData();
+// databaseSetup.addTestData();
+
+
+module.exports = databaseSetup;
