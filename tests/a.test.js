61a46d7a879c662c0182eb77613c8475abc41138
Jack Branch <jackbranch@ECS0821.local>
2018-11-13 10:30:39 +0000

    adding tests

--- tests/a.test.js
+++
@@ -1,6 +0,0 @@
-
-describe('Database Integration Tests', function() {
-    it('Connects to the Db', function() {
-        expect(true).toBe(true);
-    });
-});
---
+++ tests/dBintegration.test.js
@@ -0,0 +1,33 @@
+/**
+ * This file contains all integration tests simulating cxonnections betweenm the server and 
+ * database
+ * 
+ */
+describe('Database Integration Tests', function() {
+
+	beforeAll(function() {
+		
+	});
+
+	afterAll(function() {
+
+	});
+
+	beforeEach(function() {
+
+	});
+
+	afterEach(function() {
+
+	});
+
+	it('Server should connect to the Db', function() {
+		
+	});
+	it('Server should send a document', function() {
+
+	});
+	it('Server should recievce an document', function() {
+
+	});
+});
