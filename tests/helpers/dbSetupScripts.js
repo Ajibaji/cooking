c422b52629ab99694224382c70beea35256a48ba
Jack Branch <jackbranch@ECS0821.local>
2018-11-15 11:49:29 +0000

    fixed linting, added logger to replace console log

--- tests/helpers/dbSetupScripts.js
+++ tests/helpers/dbSetupScripts.js
@@ -9,6 +9,8 @@ let recipes = db.get('recipes');
 let stubIdUser = new User('testUser1', 'testPassword1', 'testFirstName1', 'testLastName1');
 let stubIdRecipe = new Recipe('testRecipe1', 15, 'squash', 'flattern witrh a mallet');
 
+let logger = require('logger').createLogger();
+
 let testUserList = [
 	stubIdUser,
 	new User('testUser3', 'testPassword3', 'testFirstName3', 'testLastName3'),
@@ -25,24 +27,24 @@ var databaseSetup = {
 	addTestData: function() {
 		stubIdUser.setId('5bec4b21d78d282e16f87415');
 		stubIdRecipe.set_id('5bec4b21d78d282e16f87415');
-		stubIdRecipe.setUserId('j:\"5bec4b21d78d282e16f87415\"');
+		stubIdRecipe.setUserId('j:\'5bec4b21d78d282e16f87415\'');
 		users.insert(testUserList)
 			.catch((err) => {
-				// console.log(err);
+				logger.error(err);
 			}).then(() => db.close());
 		recipes.insert(testRecipeList)
 			.catch((err) => {
-				// console.log(err);
+				logger.error(err);
 			}).then(() => db.close());
 	},
 	clearTestData: function() {
 		users.drop()
 			.catch((err) => {
-				// console.log(err);
+				logger.error(err);
 			}).then(() => db.close());
 		recipes.drop()
 			.catch((err) => {
-				// console.log(err);
+				logger.error(err);
 			}).then(() => db.close());
 	},
 };
